3d4fe0d91817a96065c9e725bb66a3e4663a80ca
Ivan Katkov <pseudokote@gmail.com>
2022-05-17 16:13:01 +0300

    Ansible_roles_add

--- pars_state.py
+++ pars_state.py
@@ -1,19 +1,11 @@
-import json,hcl
+import json
 
-'''
+from pprint import pprint
+
+
 with open ("terraform.tfstate","r") as state:
-    out = json.load(state)
+    out = json.load(state)["resources"]
 
-for item in out["resources"]:
-    for k,v in item.items():
-        if v == "vcd_nsxv_dnat":
-           for it in item["instances"]:
-               for k2,v2 in it.items():
-                    if k2 == "attributes":
-                        print(v2["original_port"],v2["translated_address"])
-'''
+for k in out:
+    pprint(k["type"])
 
-with open("variables.tf", "r") as fp:
-    obj = hcl.load(fp)
-
-print(obj)
